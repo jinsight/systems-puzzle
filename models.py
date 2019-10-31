849c49b77a2f5badff6b7ad0f4706ddd0e731850
Jayakrishnan Parappalliyalil <jparap1@homeoffice.wal-mart.com>
2019-10-30 20:47:58 -0700

    system puzzle

---
+++ models.py
@@ -0,0 +1,15 @@
+from database import Base
+from sqlalchemy import Column, Integer, String,Sequence
+from sqlalchemy.types import DateTime
+
+
+class Items(Base):
+    """
+    Items table
+    """
+    __tablename__ = 'items'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(256))
+    quantity = Column(Integer)
+    description = Column(String(256))
+    date_added = Column(DateTime())
